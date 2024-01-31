5dfc100a24ddbca03326e3345e8df2c900470391
nadiyahall <nadiyahall@protonmail.com>
2024-01-31 12:13:42 +0000

    initial

--- validate.py
+++ validate.py
@@ -20,14 +20,13 @@ def validate_address(input_file, abp_file, output_file):
     abp_df = pd.read_csv(abp_data, encoding='utf-8').rename(columns={'POSTCODE':'Postcode'})[['Postcode', 'STREET_NAME']].drop_duplicates()
 
     # Merge data frames on postcode and set 'Street_In_Postcode' True where street name in address, 'No data available'
-    # where postcode is missing from abp data, False otherwise. Vectorize for efficiency.
+    # where postcode is missing from abp data, 'No street in address' where street is blank in abp data, False otherwise.
+    # Vectorized for efficiency.
 
     merged_df = pd.merge(input_df, abp_df, how='left', on='Postcode', indicator=True)
     merged_df['Street_In_Postcode'] = np.vectorize(lambda street, address: street.lower() in address.lower())(merged_df['STREET_NAME'].astype(str), merged_df['Address'])
     merged_df['Street_In_Postcode'] = np.where(merged_df['STREET_NAME'].isnull(), 'No street in address',merged_df['Street_In_Postcode'])
     merged_df['Street_In_Postcode'] = np.where(merged_df['_merge']=='left_only', 'No data available', merged_df['Street_In_Postcode'])
-
-
 
 
     # Drop duplicates based on 'Address' and keep the first occurrence (which has 'True' prioritized)
@@ -38,8 +37,6 @@ def validate_address(input_file, abp_file, output_file):
     # Output csv.
     merged_df.drop(['Address', '_merge'], axis=True, inplace=True)
     merged_df.to_csv(output_file, index=False, encoding='utf-8')
-
-
 
 
 if __name__ == "__main__":
